fa2b323e3ed8101031e8d9a3867686b44a7a2675
LucasGabryellll <gabryelmonteiro1@gmail.com>
2024-02-09 10:28:51 -0300

    configurando deploy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/JOGO-IHM/tree/main/frontend",
   server: {
     port: 80
   }
